46d8fcd2fcb8f3e2c8ad41e5d0e98712b2582157
cokemine <aptx4561@gmail.com>
2021-08-19 01:08:58 +0800

    refactor: set default path of database

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,10 @@
-import { resolve } from 'path';
+import { platform } from 'os';
+import { mkdirSync } from 'fs';
+import { resolve, dirname } from 'path';
 import { program } from 'commander';
 
 program
-  .option('-db, --database <db>', 'the path of database', resolve(__dirname, '../db.sqlite'))
+  .option('-db, --database <db>', 'the path of database', platform() === 'win32' ? resolve(__dirname, '../db.sqlite') : '/usr/local/nodestatus/db.sqlite')
   .option('-p, --port <port>', 'the port of NodeStatus', '35601')
   .option('-i, --interval <interval>', 'update interval', '1500')
   .parse(process.argv);
@@ -11,6 +13,8 @@ const options = program.opts();
 process.env.db = options.database;
 process.env.port = options.port;
 process.env.interval = options.interval;
+
+mkdirSync(dirname(options.database), { recursive: true });
 
 import db from './lib/db';
 
