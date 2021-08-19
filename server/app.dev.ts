3028aecaec99b4d61baa4d3e6f6b37562d110f83
cokemine <aptx4561@gmail.com>
2021-08-20 01:07:38 +0800

    refactor: refactor dev middleware

--- server/app.dev.ts
+++ server/app.dev.ts
@@ -7,21 +7,26 @@ import { createIO } from './lib/io';
 import { logger } from './lib/utils';
 import config from './lib/config';
 
-const getVueWebpackConfig = (name: string) => {
-  process.env.VUE_CLI_CONTEXT = resolve(__dirname, `../web/${ name }`);
-  return require(`../web/${ name }/node_modules/@vue/cli-service/webpack.config`);
+const webpackMiddleware = async (name: string) => {
+  const pkg = await import(`../web/${ name }/package.json`);
+  if (pkg['devDependencies']['@vue/cli-service']) {
+    process.env.VUE_CLI_CONTEXT = resolve(__dirname, `../web/${ name }`);
+    return koaWebpack({
+      config: require(`../web/${ name }/node_modules/@vue/cli-service/webpack.config`),
+      devMiddleware: {
+        publicPath: '/'
+      }
+    });
+  }
+  /* WIP */
+  return null as any;
 };
 
 (async () => {
   const app = new Koa();
   const server = new Server(app.callback());
 
-  app.use(mount('/', await koaWebpack({
-    config: getVueWebpackConfig('hotaru-theme'),
-    devMiddleware: {
-      publicPath: '/'
-    }
-  })));
+  app.use(mount('/', await webpackMiddleware('hotaru-theme')));
 
   await createIO(server);
 
