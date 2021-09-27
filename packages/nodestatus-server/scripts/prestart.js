c7efd7b2c2d0f4fa695da06534f17d21ecae14f8
cokemine <aptx4561@gmail.com>
2021-09-27 23:12:19 +0800

    refactor

--- packages/nodestatus-server/scripts/prestart.js
+++ packages/nodestatus-server/scripts/prestart.js
@@ -1,15 +1,10 @@
 /* Initializing database for Docker */
 const cp = require('child_process');
 const { resolve, dirname } = require('path');
-const { homedir, platform } = require('os');
 const fs = require('fs');
 
 if (process.env.IS_DOCKER === 'true') {
-  let dbPath = process.env.DATABASE || (
-    platform() === 'win32'
-      ? resolve(homedir(), '.nodestatus/db.sqlite')
-      : '/usr/local/NodeStatus/server/db.sqlite'
-  );
+  let dbPath = process.env.DATABASE || '/usr/local/NodeStatus/server/db.sqlite';
   fs.mkdirSync(dirname(dbPath), { recursive: true });
   cp.spawn('prisma', [ 'db', 'push', '--accept-data-loss', '--skip-generate' ], {
     env: { BINARY_TARGETS: '["native"]', DATABASE_URL: `file:${dbPath}`, ...process.env },
