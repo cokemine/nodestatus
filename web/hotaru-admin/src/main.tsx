57120ea838448e7bb598c9ec0a2f7fa7996da15f
cokemine <aptx4561@gmail.com>
2021-10-14 11:30:32 +0800

    refactor

--- web/hotaru-admin/src/main.tsx
+++ web/hotaru-admin/src/main.tsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 // eslint-disable-next-line import/no-unresolved
 import 'virtual:svg-icons-register';
-import ids from 'virtual:svg-icons-names';
-
-console.log(ids);
 
 ReactDOM.render(
   <React.StrictMode>
