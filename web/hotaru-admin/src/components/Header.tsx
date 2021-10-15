3256997645cae365c1cb8c8696d000e07eb5c805
cokemine <aptx4561@gmail.com>
2021-10-15 23:05:22 +0800

    feat(hotaru-admin): add logout menu

--- web/hotaru-admin/src/components/Header.tsx
+++ web/hotaru-admin/src/components/Header.tsx
@@ -1,6 +1,9 @@
 import React, { FC } from 'react';
-import { Avatar } from 'antd';
-import { UserOutlined, MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
+import { Avatar, Dropdown, Menu } from 'antd';
+import {
+  UserOutlined, MenuFoldOutlined, MenuUnfoldOutlined, LogoutOutlined
+} from '@ant-design/icons';
+import { useHistory } from 'react-router-dom';
 
 interface Props {
   collapsed: {
@@ -10,14 +13,31 @@ interface Props {
 }
 
 const Header: FC<Props> = props => {
+  const history = useHistory();
   const { isCollapsed, toggleCollapsed } = props.collapsed;
+
+  const menu = (
+    <Menu>
+      <Menu.Item onClick={() => {
+        localStorage.removeItem('token');
+        history.push('/login');
+      }}
+      >
+        <LogoutOutlined className="mr-2 align-middle" />
+        <span className="align-middle">Logout</span>
+      </Menu.Item>
+    </Menu>
+  );
+
   return (
     <div className="h-full flex items-center justify-between">
       {React.createElement(isCollapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
         className: 'text-2xl',
         onClick: toggleCollapsed
       })}
-      <Avatar size={40} icon={<UserOutlined />} />
+      <Dropdown overlay={menu} placement="bottomCenter">
+        <Avatar size={40} icon={<UserOutlined />} />
+      </Dropdown>
     </div>
   );
 };
