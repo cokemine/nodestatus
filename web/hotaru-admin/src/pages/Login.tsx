05b9b38537bcfc7e800a91b6e12dd7d8a8047db5
cokemine <aptx4561@gmail.com>
2021-10-15 23:19:57 +0800

    refactor(hotaru-admin): login page

--- web/hotaru-admin/src/pages/Login.tsx
+++ web/hotaru-admin/src/pages/Login.tsx
@@ -27,9 +27,9 @@ const Login: FC = () => {
 
   return (
     <div className="flex items-center min-h-screen p-6 bg-gray-50">
-      <div className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl">
-        <div className="flex flex-col overflow-y-auto md:flex-row">
-          <div className="h-32 md:h-auto md:w-1/2">
+      <div className="flex-1 h-full max-w-xl md:max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl">
+        <div className="flex flex-col md:flex-row">
+          <div className="h-60 md:h-auto md:w-1/2">
             <img
               aria-hidden="true"
               className="object-cover w-full h-full"
@@ -37,7 +37,7 @@ const Login: FC = () => {
               alt="Office"
             />
           </div>
-          <main className="flex flex-col items-center justify-center p-6 sm:p-16 md:w-1/2">
+          <div className="flex flex-col items-center justify-center p-6 sm:p-16 md:w-1/2">
             <h1 className="text-2xl font-semibold text-gray-700 mb-6">NodeStatus</h1>
             <Form
               className="w-full"
@@ -68,7 +68,7 @@ const Login: FC = () => {
                 </Button>
               </Form.Item>
             </Form>
-          </main>
+          </div>
         </div>
       </div>
     </div>
