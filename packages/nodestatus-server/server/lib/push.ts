87614ed68ca00a4eb625165ff91becfdc6c90955
cokemine <aptx4561@gmail.com>
2021-11-12 12:54:20 +0800

    fix: fix telegram push panic in group
    fix #52

--- packages/nodestatus-server/server/lib/push.ts
+++ packages/nodestatus-server/server/lib/push.ts
@@ -80,9 +80,9 @@ export default function createPush(this: NodeStatus, options: PushOptions) {
     bot.command('start', ctx => {
       const currentChat = ctx.message.chat.id.toString();
       if (chatId.has(currentChat)) {
-        ctx.reply(`🍊NodeStatus\n🤖 Hi, this chat id is *${currentChat}*\\.\nYou have access to this service\\. I will alert you when your servers changed\\.\nYou are currently using NodeStatus: *${parseEntities(process.env.npm_package_version)}*`, { parse_mode: 'MarkdownV2' });
+        ctx.reply(`🍊NodeStatus\n🤖 Hi, this chat id is *${parseEntities(currentChat)}*\\.\nYou have access to this service\\. I will alert you when your servers changed\\.\nYou are currently using NodeStatus: *${parseEntities(process.env.npm_package_version)}*`, { parse_mode: 'MarkdownV2' });
       } else {
-        ctx.reply(`🍊NodeStatus\n🤖 Hi, this chat id is *${currentChat}*\\.\nYou *do not* have permission to use this service\\.\nPlease check your settings\\.`, { parse_mode: 'MarkdownV2' });
+        ctx.reply(`🍊NodeStatus\n🤖 Hi, this chat id is *${parseEntities(currentChat)}*\\.\nYou *do not* have permission to use this service\\.\nPlease check your settings\\.`, { parse_mode: 'MarkdownV2' });
       }
     });
 
