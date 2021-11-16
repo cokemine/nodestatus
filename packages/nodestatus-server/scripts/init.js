332a5fed53e04cdd463d45c5d4145e5a4a39892b
cokemine <aptx4561@gmail.com>
2021-11-16 13:30:24 +0800

    chore: add comments

--- packages/nodestatus-server/scripts/init.js
+++ packages/nodestatus-server/scripts/init.js
@@ -1,3 +1,10 @@
+/*
+*  Prisma 在 https://github.com/prisma/prisma/issues/3834 移除了对多 Provider 的支持。
+*  这使得我们需要在每次运行时重新根据 DATABASE_URL 手动修改 schema
+*  并重新生成 @prisma/client 来实现修改成正确的 Provider
+*  同时重新调用 `prisma db push` 来实现类似于 Sequelize 的 sync 效果 来应对可能的数据库结构变化
+*  如果有更好的方案欢迎提出
+* */
 const fs = require('fs');
 const { platform, homedir } = require('os');
 const { resolve } = require('path');
