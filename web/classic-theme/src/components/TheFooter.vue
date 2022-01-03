d66a8da3a64eedb6d68bd0efe87c39bc00b211ce
cokemine <aptx4561@gmail.com>
2022-01-03 14:10:14 +0800

    modify header and footer style

--- web/classic-theme/src/components/TheFooter.vue
+++ web/classic-theme/src/components/TheFooter.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="container">
-    <p style="text-align: center; font-size: 14px; letter-spacing: .08rem;">
+    <p class="footer">
       Powered by <a href="https://github.com/cokemine/nodestatus">NodeStatus</a>
     </p>
   </div>
 </template>
+
+<style>
+.footer {
+  text-align: center;
+  font-size: 14px;
+  letter-spacing: .08rem;
+}
+</style>
--- web/classic-theme/src/components/TheHeader.vue
+++ web/classic-theme/src/components/TheHeader.vue
@@ -5,48 +5,18 @@
         <div class="navbar-header">
           <a href="#" class="navbar-brand">NodeStatus</a>
         </div>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="dropdown">
-              <a data-toggle="dropdown" class="dropdown-toggle" href="#">风格<b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li><a href="#" @click="setActiveStyleSheet('dark')">黑夜</a></li>
-                <li><a href="#" @click="setActiveStyleSheet('light')">白天</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div><!--/.nav-collapse -->
       </div>
     </div>
   </div>
 </template>
-
-<script lang="ts" setup>
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const setActiveStyleSheet = (theme: string): void => {};
-
-</script>
 
 <style>
 .navbar {
   min-height: 40px;
 }
 
-.navbar-brand {
-  color: #fff;
-  padding: 10px;
-  font-size: 20px;
-}
-
-.dropdown .dropdown-toggle {
-  padding-bottom: 10px;
-  padding-top: 10px;
-}
-
 .navbar-inverse .navbar-brand {
   color: #fff;
-  padding: 10px;
   font-size: 20px;
 }
 </style>
